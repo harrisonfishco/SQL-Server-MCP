b36d11169e3268aef159b6219bef79de2d81919f
Harrison Fishco <hfishco@harrisonfishco.com>
2026-04-08 18:33:01 -0700

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace SQL_Server_MCP
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Logging.AddConsole(consoleLogOptions =>
+            {
+                consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+            });
+
+            builder.Services
+                .AddMcpServer()
+                .WithStdioServerTransport()
+                .WithToolsFromAssembly();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
